6f9dd828eed08af8f981894d1deab9ec8d3183d6
Yasuharu Nakano <ynak@jggug.org>
2016-03-23 12:12:25 +0900

    Add configurations to emit logs of Groovy SQL

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,12 @@
+import groovy.sql.Sql
+import java.util.logging.Level
+
 class BootStrap {
 
     def init = { servletContext ->
+        Sql.LOG.level = Level.FINE
     }
+
     def destroy = {
     }
 }
--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -85,10 +85,8 @@ grails.hibernate.pass.readonly = false
 // configure passing read-only to OSIV session by default, requires "singleSession = false" OSIV mode
 grails.hibernate.osiv.readonly = false
 
+grails.logging.jul.usebridge = true
 environments {
-    development {
-        grails.logging.jul.usebridge = true
-    }
     production {
         grails.logging.jul.usebridge = false
         // TODO: grails.serverURL = "http://www.changeme.com"
